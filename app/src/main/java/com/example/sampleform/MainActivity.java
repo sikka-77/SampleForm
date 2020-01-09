eca36b6cf5fb3c1767559d0b120f25a2ef345d87
Bhavya sikka <43481116+sikka-77@users.noreply.github.com>
2020-01-09 15:41:22 +0530

    firebase login signup added

--- app/src/main/java/com/example/sampleform/MainActivity.java
+++ app/src/main/java/com/example/sampleform/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.sampleform;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView mRegisterText;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private Button mSignInButton;
+    private FirebaseAuth firebaseSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRegisterText=(TextView)findViewById(R.id.Register);
+        mEmailEditText=(EditText)findViewById(R.id.getUserEmail);
+        mPasswordEditText=(EditText)findViewById(R.id.getUserPassword);
+        mSignInButton=(Button)findViewById(R.id.Signinside);
+        firebaseSignin=FirebaseAuth.getInstance();
+
+        mSignInButton.setOnClickListener(this);
+
 
         //setting the click listener
 
@@ -28,7 +49,41 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    private void userLogin(){
+        String email=mEmailEditText.getText().toString().trim();
+        String password=mPasswordEditText.getText().toString().trim();
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please set a password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        firebaseSignin.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Signed In", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Try Again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
+
+    public void onClick(View view){
+        if(view==mSignInButton)
+        {
+            userLogin();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/sampleform/SignUpActivity.java
+++ app/src/main/java/com/example/sampleform/SignUpActivity.java
@@ -1,20 +1,45 @@
 package com.example.sampleform;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView mSignin;
+    private EditText mNameEditText;
+    private EditText mEmailEditText;
+    private EditText mMobileEditText;
+    private EditText mLocationEditText;
+    private EditText mPasswordEditText;
+    private Button mSignUpButton;
+    private FirebaseAuth firebaseSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mNameEditText=(EditText)findViewById(R.id.getNameEditText);
+        mEmailEditText=(EditText)findViewById(R.id.getEmailEditText);
+        mMobileEditText=(EditText)findViewById(R.id.getMobileEditText);
+        mLocationEditText=(EditText)findViewById(R.id.getLocationEditText);
+        mPasswordEditText=(EditText)findViewById(R.id.getPasswordEditText);
+        mSignUpButton=(Button)findViewById(R.id.SignUpButton);
+        firebaseSignup=FirebaseAuth.getInstance();
 
         mSignin=(TextView)findViewById(R.id.Signin);
         mSignin.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +49,46 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        mSignUpButton.setOnClickListener(this);
     }
+
+    private void registerNewUser()
+    {
+        String email=mEmailEditText.getText().toString().trim();
+        String password=mPasswordEditText.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please set a password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+//creating new user
+
+        firebaseSignup.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SignUpActivity.this, "account created", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this, "Try Again" , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void onClick(View view)
+    {
+        if(view==mSignUpButton)
+        {
+            registerNewUser();
+        }
+    }
+
 }
